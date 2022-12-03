a9da87c20a4a514c37188b2b17a212f82fbab9d1
akferreira <49925514+akferreira@users.noreply.github.com>
2022-12-03 15:57:08 -0300

    Adiciona teste de solvabilidade do jogo

--- jogo8.py
+++ jogo8.py
@@ -20,22 +20,46 @@ TEMP = 'X'
 #4 5 6
 #7 8 9
 
+#1 2 3
+#4 5 6
+#7 8 0
+
+#2 0 3
+#4 5 1
+#6 8 7
+#23541687
+#12345678
+
+
 
 vizinhos = {
     0: [(3,Acao.ABAIXO),(1,Acao.DIREITA)],
     1: [(0,Acao.ESQUERDA),(2,Acao.DIREITA),(4,Acao.ABAIXO)],
-    2: [(1,Acao.ESQUERDA),(5, Acao.ABAIXO)],
-    3: [(6,Acao.ABAIXO),(0,Acao.ACIMA),(4,Acao.DIREITA)],
-    4: [(1,Acao.ACIMA),(5,Acao.DIREITA),(7,Acao.ABAIXO),(3,Acao.ESQUERDA)],
-    5: [(4,Acao.ESQUERDA),(2,Acao.ACIMA),(8,Acao.ABAIXO)],
+    2: [(5, Acao.ABAIXO),(1,Acao.ESQUERDA)],
+    3: [(6,Acao.ABAIXO),(4,Acao.DIREITA),(0,Acao.ACIMA)],
+    4: [(1,Acao.ACIMA),(5,Acao.DIREITA),(3,Acao.ESQUERDA),(7,Acao.ABAIXO)],
+    5: [(8,Acao.ABAIXO),(4,Acao.ESQUERDA),(2,Acao.ACIMA)],
     6: [(7,Acao.DIREITA),(3,Acao.ACIMA)],
-    7: [(8,Acao.DIREITA),(6,Acao.ESQUERDA),(4,Acao.ACIMA)],
-    8: [(5,Acao.ACIMA),(7,Acao.ESQUERDA)]
+    7: [(4,Acao.ACIMA),(6,Acao.ESQUERDA),(8,Acao.DIREITA)],
+    8: [(7,Acao.ESQUERDA),(5,Acao.ACIMA)]
 
 }
 
+def invCount(tabuleiro):
+    arr = tabuleiro.replace(VAZIO,'0')
+    inv_count = 0
+    for i in range(9):
+        for j in range(i+1,9):
+            if ( (int(arr[j]) and (int(arr[i]))) and (int(arr[i]) > int(arr[j])) ):
+                inv_count+=1
+
+    return inv_count
+
+
 def sucessor(estado_atual):
+    #print(f"{estado_atual.index(VAZIO)}")
     sucessores = []
+
     pos_vazio = estado_atual.index(VAZIO)
     jogadas = vizinhos[pos_vazio]
 
@@ -50,20 +74,33 @@ def sucessor(estado_atual):
 
     return sucessores
 
+
+
+print(invCount('123456_87'))
+print(invCount('2_3541687'))
+
 #print(sucessor('2_3541687'))
-inicial = '41372586_'
+inicial = sucessor('123456_87')
 next = inicial
-for i in range(180000):
-    next = sucessor(next)[0][1]
+
+
+for x in range(200000):
+    try:
+        current = next.pop()
+        if(current[1] == "12345678_"):
+            print("sucess")
+            print(i*x)
+            break
+        else:
+            for resultado in sucessor(current[1]):
+                next.append(resultado)
+    except IndexError:
+        next = current
+        break
 
     if(next == "12345678_"):
         print("sucess")
-        print(i)
+        print(i*x)
         break
 
-
 print(next)
-
-
-
-
